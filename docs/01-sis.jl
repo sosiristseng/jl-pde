2aa8346911390de4afde6ac36629c441d326ebc1
Wen Wei Tseng <sosiristseng@gmail.com>
2024-04-15 16:26:13 +0800

    Literate notebooks (#15)

---
+++ docs/01-sis.jl
@@ -0,0 +1,102 @@
+#===
+# 1D PDE: SIS diffusion model
+
+$$
+\begin{align}
+\frac{\partial S}{\partial t} &= d_S S_{xx} - \beta(x)\frac{SI}{S+I} + \gamma(x)I  \\
+\frac{\partial I}{\partial t} &= d_I I_{xx} + \beta(x)\frac{SI}{S+I} - \gamma(x)I
+\end{align}
+$$
+
+where $x \in (0, 1)$
+
+Solve the steady-state problem $\frac{\partial S}{\partial t} = \frac{\partial I}{\partial t} = 0$
+
+The boundary condition: $\frac{\partial S}{\partial x} = \frac{\partial I}{\partial x} = 0$ for x = 0, 1
+
+The conservative relationship: $\int^{1}_{0} (S(x) + I(x) ) dx = 1$
+
+Notations:
+- $x$ : location
+- $t$ : time
+- $S(x, t)$ : the density of susceptible populations
+- $I(x, t)$ : the density of infected populations
+- $d_S$ / $d_I$ : the diffusion coefficients for susceptible and infected individuals
+- $\beta(x)$ : transmission rates
+- $\gamma(x)$ : recovery rates
+===#
+
+using DifferentialEquations
+using ModelingToolkit
+using MethodOfLines
+using DomainSets
+using Plots
+
+# Setup parameters, variables, and differential operators
+@parameters t x
+@parameters dS dI brn ϵ
+@variables S(..) I(..)
+Dt = Differential(t)
+Dx = Differential(x)
+Dxx = Differential(x)^2
+
+# Helper functions
+γ(x) = x + 1
+ratio(x, brn, ϵ) =  brn + ϵ * sinpi(2x)
+
+# 1D PDE and boundary conditions
+eqs = [
+    Dt(S(t, x)) ~ dS * Dxx(S(t, x)) - ratio(x, brn, ϵ) * γ(x) * S(t, x) * I(t, x) / (S(t, x) + I(t, x)) + γ(x) * I(t, x),
+    Dt(I(t, x)) ~ dI * Dxx(I(t, x)) + ratio(x, brn, ϵ) * γ(x) * S(t, x) * I(t, x) / (S(t, x) + I(t, x)) - γ(x) * I(t, x)
+]
+
+# Boundary conditions
+bcs = [
+    S(0, x) ~ 0.9 + 0.1 * sinpi(2x),
+    I(0, x) ~ 0.1 + 0.1 * cospi(2x),
+    Dx(S(t, 0)) ~ 0.0,
+    Dx(S(t, 1)) ~ 0.0,
+    Dx(I(t, 0)) ~ 0.0,
+    Dx(I(t, 1)) ~ 0.0
+]
+
+# Space and time domains
+domains = [
+    t ∈ Interval(0.0, 10.0),
+    x ∈ Interval(0.0, 1.0)
+]
+
+# Define the PDE system
+@named pdesys = PDESystem(eqs, bcs, domains,
+    [t, x], ## Independent variables
+    [S(t, x), I(t, x)],  ## Dependent variables
+    [dS => 0.5, dI => 0.1, brn => 3, ϵ => 0.1] ## Initial conditions
+)
+
+# Method of lines discretization
+# Finite difference method (FDM) converts the PDE system into an ODE problem
+dx = 0.01
+order = 2
+discretization = MOLFiniteDifference([x => dx], t, approx_order=order)
+prob = discretize(pdesys, discretization)
+
+# ## Solving time-dependent SIS epidemic model
+# `KenCarp4` is good at solving semilinear problems (like this one).
+sol = solve(prob, KenCarp4(), saveat=0.2)
+
+# Retrieve the results (Matrices) and visulize them
+discrete_x = sol[x]
+discrete_t = sol[t]
+S_solution = sol[S(t, x)]
+I_solution = sol[I(t, x)]
+
+surface(discrete_x, discrete_t, S_solution, xlabel="Location", ylabel="Time", title="Susceptible")
+
+#---
+surface(discrete_x, discrete_t, I_solution, xlabel="Location", ylabel="Time", title="Infectious")
+
+# ## Solving the steady state problem
+steadystateprob = SteadyStateProblem(prob)
+sssol = solve(steadystateprob, DynamicSS(TRBDF2()))
+I_solution = sssol[I(t, x)]
+S_solution = sssol[S(t, x)]
---
+++ docs/02-heat.jl
@@ -0,0 +1,70 @@
+#===
+# 2D heat equation
+
+From the tutorial
++ https://docs.sciml.ai/MethodOfLines/stable/tutorials/heatss/
++ https://docs.sciml.ai/MethodOfLines/stable/tutorials/heat/
+
+Using `MethodOfLines.jl` (https://github.com/SciML/MethodOfLines.jl/) to symbolically define the PDE system and use the [finite difference method](https://en.wikipedia.org/wiki/Finite_difference_method) (FDM) to solve the following PDE:
+
+$$
+\frac{\partial u}{\partial t} = \frac{\partial^2 u}{\partial x^2} + \frac{\partial^2 u}{\partial y^2}
+$$
+===#
+
+using ModelingToolkit
+using MethodOfLines
+using DomainSets
+using OrdinaryDiffEq
+using Plots
+
+# Setup variables and differential operators
+@variables t x y
+@variables u(..)
+
+Dt = Differential(t)
+Dxx = Differential(x)^2
+Dyy = Differential(y)^2
+
+# PDE equation
+eq = Dt(u(t, x, y)) ~ Dxx(u(t, x, y)) + Dyy(u(t, x, y))
+
+# Boundary conditions
+bcs = [
+    u(0, x, y) ~ 0,
+    u(t, 0, y) ~ x * y,
+    u(t, 1, y) ~ x * y,
+    u(t, x, 0) ~ x * y,
+    u(t, x, 1) ~ x * y,
+]
+
+# Space and time domains
+domains = [
+    t ∈ Interval(0.0, 1.0),
+    x ∈ Interval(0.0, 1.0),
+    y ∈ Interval(0.0, 1.0)
+]
+
+# PDE system
+@named pdesys = PDESystem(eq, bcs, domains, [t, x, y], [u(t, x, y)])
+
+# discretize the PDE system
+N = 20
+discretization = MOLFiniteDifference([x=>N, y=>N], t, approx_order=2, grid_align=MethodOfLines.EdgeAlignedGrid())
+prob = discretize(pdesys, discretization)
+
+# Solve the PDE
+sol = solve(prob, KenCarp4(), saveat=0.01)
+
+# Extract data
+discrete_x = sol[x]
+discrete_y = sol[y]
+discrete_t = sol[t]
+solu = sol[u(t, x, y)]
+
+# Animate the solution
+anim = @animate for k in eachindex(discrete_t)
+    heatmap(solu[k, 2:end-1, 2:end-1], title="u @ t=$(discrete_t[k])", aspect_ratio=:equal)
+end
+
+mp4(anim,"2Dheat.mp4", fps = 8)
