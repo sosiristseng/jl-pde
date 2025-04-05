756850c8a296d4f61987fd89ddb66697550692ba
Wen Wei Tseng <sosiristseng@gmail.com>
2025-04-05 23:44:28 +0800

    smaller problem

--- docs/ode-pde.jl
+++ docs/ode-pde.jl
@@ -30,20 +30,20 @@ function build_brussilator(; N::Int=32, x_min=0, y_min=0, t_min=0, x_max=1, y_ma
     defaults = Dict()
     eqs = []
     for i in 1:N, j in 1:N
-        x = (j-1) * dx
-        y = (i-1) * dy
+        x = (j - 1) * dx
+        y = (i - 1) * dy
         uterm = 1.0 + v[i, j] * u[i, j]^2 - 4.4 * u[i, j] + brusselator_f(x, y, t)
         vterm = 3.4 * u[i, j] - v[i, j] * u[i, j]^2
         ## No flux boundary conditions
-        i_prev = ifelse(i==1, i, i-1)
-        i_next = ifelse(i==N, i, i+1)
-        j_prev = ifelse(j==1, j, j-1)
-        j_next = ifelse(j==N, j, j+1)
+        i_prev = ifelse(i == 1, i, i - 1)
+        i_next = ifelse(i == N, i, i + 1)
+        j_prev = ifelse(j == 1, j, j - 1)
+        j_next = ifelse(j == N, j, j + 1)
         udiff = (u[i, j_prev] - 2u[i, j] + u[i, j_next]) / dx^2 + (u[i_prev, j] - 2u[i, j] + u[i_next, j]) / dy^2
         vdiff = (v[i, j_prev] - 2v[i, j] + v[i, j_next]) / dx^2 + (v[i_prev, j] - 2v[i, j] + v[i_next, j]) / dy^2
         append!(eqs, [Dt(u[i, j]) ~ α * udiff + uterm, Dt(v[i, j]) ~ α * vdiff + vterm])
         defaults[u[i, j]] = _u0(x, y)
-        defaults[v[i, j]] =  _v0(x, y)
+        defaults[v[i, j]] = _v0(x, y)
     end
     sys = ODESystem(eqs, t; name, defaults)
     if simplify
@@ -53,6 +53,6 @@ function build_brussilator(; N::Int=32, x_min=0, y_min=0, t_min=0, x_max=1, y_ma
 end
 
 #---
-@time "Build system" sys = build_brussilator(simplify=false) |> complete
-@time "Build ODE problem" prob = ODEProblem(sys, [], 11.5; sparse=true)
-@time "Solve the problem" sol = solve(prob, TRBDF2(), saveat=0.1)
+@time "Build system" sys = build_brussilator(N=20, simplify=false) |> complete
+@time "Build ODE problem" prob = ODEProblem(sys, [], 11.5; sparse=true, jac=true)
+@time "Solve the problem" sol = solve(prob, KenCarp47(), saveat=0.1)
