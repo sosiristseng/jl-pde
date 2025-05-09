334f17a1b9694f2fe286cbe79ac670e139625b6c
Wen Wei Tseng <sosiristseng@gmail.com>
2025-05-09 23:18:56 +0800

    No MTK

--- docs/ode-pde.jl
+++ docs/ode-pde.jl
@@ -11,48 +11,51 @@ $$
 $$
 
 ===#
-using ModelingToolkit
+import ComponentArrays as CA
+using SimpleUnPack
 using OrdinaryDiffEq
+using LinearSolve
 using Plots
 
-function build_brussilator(; N::Int=32, x_min=0, y_min=0, t_min=0, x_max=1, y_max=1, t_max=11.5, name=:sys, simplify=true)
+function build_brussilator(; N::Int=32, x_min=0, y_min=0, t_min=0, x_max=1, y_max=1, t_max=11.5, α = 10.0)
     @assert (x_min < x_max) && (y_min < y_max) && (t_min < t_max) && (N > 1)
-    @independent_variables t
-    @parameters α = 10.0
-    @variables u(t)[1:N, 1:N] v(t)[1:N, 1:N]
-    Dt = Differential(t)
-    dx = (x_max - x_min) / (N - 1)
-    dy = (y_max - y_min) / (N - 1)
-    ## Dynamics on each grid point
-    brusselator_f(x, y, t) = (((x - 0.3)^2 + (y - 0.6)^2) <= 0.1^2) * (t >= 1.1) * 5
-    _u0(x, y) = 22 * (y * (1 - y))
-    _v0(x, y) = 27 * (x * (1 - x))
-    defaults = Dict()
-    eqs = []
+    u0 = CA.ComponentArray(u = zeros(N, N), v = zeros(N, N))
+    xx = range(x_min, x_max, length=N)
+    yy = range(y_min, y_max, length=N)
+    dx = step(xx)
+    dy = step(yy)
+    ps = CA.ComponentArray(; α, xx, yy, N, dx, dy)
+
     for i in 1:N, j in 1:N
-        x = (j - 1) * dx
-        y = (i - 1) * dy
-        uterm = 1.0 + v[i, j] * u[i, j]^2 - 4.4 * u[i, j] + brusselator_f(x, y, t)
-        vterm = 3.4 * u[i, j] - v[i, j] * u[i, j]^2
-        ## No flux boundary conditions
-        i_prev = ifelse(i == 1, i, i - 1)
-        i_next = ifelse(i == N, i, i + 1)
-        j_prev = ifelse(j == 1, j, j - 1)
-        j_next = ifelse(j == N, j, j + 1)
-        udiff = (u[i, j_prev] - 2u[i, j] + u[i, j_next]) / dx^2 + (u[i_prev, j] - 2u[i, j] + u[i_next, j]) / dy^2
-        vdiff = (v[i, j_prev] - 2v[i, j] + v[i, j_next]) / dx^2 + (v[i_prev, j] - 2v[i, j] + v[i_next, j]) / dy^2
-        append!(eqs, [Dt(u[i, j]) ~ α * udiff + uterm, Dt(v[i, j]) ~ α * vdiff + vterm])
-        defaults[u[i, j]] = _u0(x, y)
-        defaults[v[i, j]] = _v0(x, y)
+        x = xx[j]
+        y = yy[i]
+        u0.u[i, j] = 22 * (y * (1 - y))
+        u0.v[i, j] = 27 * (x * (1 - x))
     end
-    sys = ODESystem(eqs, t; name, defaults)
-    if simplify
-        sys = structural_simplify(sys)
+
+    odef = (ds, s, p, t) -> begin
+        @unpack α, xx, yy, dx, dy = p
+        @unpack u, v = s
+        brusselator_f(x, y, t) = (((x - 0.3)^2 + (y - 0.6)^2) <= 0.1^2) * (t >= 1.1) * 5
+        for (i, y) in enumerate(yy), (j, x) in enumerate(xx)
+            uterm = 1.0 + v[i, j] * u[i, j]^2 - 4.4 * u[i, j] + brusselator_f(x, y, t)
+            vterm = 3.4 * u[i, j] - v[i, j] * u[i, j]^2
+            ## No flux boundary conditions
+            i_prev = clamp(i - 1, 1, N)
+            i_next = clamp(i + 1, 1, N)
+            j_prev = clamp(j - 1, 1, N)
+            j_next = clamp(j + 1, 1, N)
+            unabla = (u[i, j_prev] - 2u[i, j] + u[i, j_next]) / dx^2 + (u[i_prev, j] - 2u[i, j] + u[i_next, j]) / dy^2
+            vnabla = (v[i, j_prev] - 2v[i, j] + v[i, j_next]) / dx^2 + (v[i_prev, j] - 2v[i, j] + v[i_next, j]) / dy^2
+            ds.u[i, j] = α * unabla + uterm
+            ds.v[i, j] = α * vnabla + vterm
+        end
     end
-    return sys
+    return (; u0, odef, ps)
 end
 
 #---
-@time "Build system" sys = build_brussilator(N=20, simplify=false) |> complete
-@time "Build ODE problem" prob = ODEProblem(sys, [], 11.5; sparse=true, jac=true)
-@time "Solve the problem" sol = solve(prob, KenCarp47(), saveat=0.1)
+@unpack u0, odef, ps = build_brussilator()
+tspan = (0.0, 11.5)
+prob = ODEProblem(odef, u0, tspan, ps)
+@time sol = solve(prob, KenCarp47(linsolve = KrylovJL_GMRES()), saveat=0.1)
